575328a9a8009eee71681f7f03dcf9b6a062c8b2
tim.galvin <gal16b@csiro.au>
2020-04-20 11:21:28 +0800

    package rename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyink-tim", # Replace with your own username
+    name="pyink",  # Replace with your own username
     version="0.0.1",
     author="Tim Galvin",
     author_email="gal16b@csiro.au",
@@ -17,10 +17,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-          'markdown',
-          'numpy',
-          'astropy',
-    ],
-    python_requires='>=3.6',
+    install_requires=["markdown", "numpy", "astropy",],
+    python_requires=">=3.6",
 )
