b3bae4ec9e84689c0c2fd7b8423562d48f212c24
tim.galvin <gal16b@csiro.au>
2020-05-08 17:04:50 +0800

    initial commit of PathHelper

--- pyink/utils.py
+++ pyink/utils.py
@@ -53,3 +53,61 @@ def pink_spatial_transform(
         raise ValueError(
             f"Image to transform must be either of shape (channel, height, width) or (height, width). Got image of shape {img.shape}"
         )
+
+
+class PathHelper:
+    """Small utility helper to transparently create output folders. Idea being:
+
+    path = PathHelpher('Example_Images')
+    path.images1
+    path.images2
+
+    will produce a folder structure of:
+    Example_Images/
+    Example_Images/images1/
+    Example_Images/images2/
+
+    This was written with creating matplotlib figures in mind across different trials
+    """
+
+    def __init__(self, path: str, clobber: bool = False) -> None:
+        """Creates the base path 
+        
+        Arguments:
+            path {str} -- Base path to create 
+        
+        Keyword Arguments:
+            clobber {bool} -- Will delete the base path and its contents if it exists (default: {False})
+        """
+        if clobber and os.path.exists(path):
+            shutil.rmtree(path)
+
+        if not os.path.exists(path):
+            os.mkdir(path)
+        self.path = path
+
+    def __getattr__(self, name: str) -> PathHelpher:
+        """Will create a subdirectory of the specified base path
+        
+        Arguments:
+            name {str} -- New sub-directory
+        
+        Returns:
+            PathHelpher -- New PathHelper object with the base path of the sub-directory
+        """
+        name_path = f"{self.path}/{name}"
+
+        if not os.path.exists(name_path) and "_ipython" not in name:
+            os.mkdir(name_path)
+
+        return self.__class__(name_path)
+
+    def __repr__(self) -> str:
+        """Neat string representation simply returning the base path
+        
+        Returns:
+            str -- The base path
+        """
+
+        return self.path
+
