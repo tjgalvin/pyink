2c706703ee5c3fc1321db3c49365b494181758b0
tim.galvin <gal16b@csiro.au>
2020-05-11 18:40:24 +0800

    example annotation script

---
+++ Example_Scripts/run_annotation_example.py
@@ -0,0 +1,36 @@
+"""Simple example driving script to interactively annotate neurons
+
+TODO: Add a argparse interface to make script callable from command line
+"""
+
+import platform
+import logging
+
+logger = logging.getLogger("pyink.annotation")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+
+plat = platform.system()
+if plat == "Darwin":
+    import matplotlib
+
+    matplotlib.use("MacOSX")
+    logger.debug(
+        "Detected MacOSX platform. Configured matplotlib backend appropriately."
+    )
+
+import pyink as pu
+
+path = pu.PathHelper("Annotation")
+
+annotator = pu.Annotator("SOMs/SOM_B3Circular_h8_w8_emu.bin")
+
+annotator.interactive_annotate()
+annotator.save_annotations(path=f"{path}/ANNOTATION_B3Circular_h8_w8_emu.bin")
+
+saved_annotations = pu.Annotator(
+    "SOMs/SOM_B3Circular_h8_w8_emu.bin",
+    results=f"{path}/ANNOTATION_B3Circular_h8_w8_emu.bin",
+)
+print(saved_annotations.results)
