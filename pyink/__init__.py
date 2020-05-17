3c24a79eefb42d11151ffb5562663846ac08a6ed
tim.galvin <gal16b@csiro.au>
2020-05-17 21:32:23 +0800

    updated the init to include new FilterSet and SOMSet

--- pyink/__init__.py
+++ pyink/__init__.py
@@ -2,7 +2,7 @@ from .preprocessing import *
 from .utils import pink_spatial_transform, PathHelper
 from .binwrap import *
 from .annotator import Annotation, Annotator
-from .filteractions import CoordinateTransformer, Filter
+from .filteractions import CoordinateTransformer, Filter, FilterSet
 from .collation import LabelResolve, Action, Sorter
 
 __author__ = "Tim Galvin"
