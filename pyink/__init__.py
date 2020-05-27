0fd93d35d16927714bc08405fb8482689133f160
tim.galvin <gal16b@csiro.au>
2020-05-27 15:58:36 +0800

    Exposed the greedy_graph function
    for profit

--- pyink/__init__.py
+++ pyink/__init__.py
@@ -3,6 +3,6 @@ from .utils import pink_spatial_transform, PathHelper
 from .binwrap import *
 from .annotator import Annotation, Annotator, ANT_SUFFIX
 from .filteractions import CoordinateTransformer, Filter, FilterSet
-from .collation import LabelResolve, Action, Sorter, Grouper
+from .collation import LabelResolve, Action, Sorter, Grouper, greedy_graph
 
 __author__ = "Tim Galvin"
